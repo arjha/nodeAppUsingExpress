5753952aeb3970058cc0c79a36a5407c08fe2ef7
Ashutosh Ranjan Jha <arjha@Ashutoshs-MacBook-Air.local>
2018-10-06 16:27:17 +0530

    second commit

---
+++ servers.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Error while writing log in the file');
+        }
+    })
+    next();
+});
+// app.use((req, res, next) => {
+//     res.render('maintinance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrYear', () => {
+    return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageInfo: 'Welcome...',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageInfo: 'This is about page',
+    });
+});
+app.get('/bad', (req, res) => {
+    res.send('<h2 style="color:red;">Bad Request</h2>');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
